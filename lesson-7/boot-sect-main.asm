6e43b7727e5e423607a4f6a19e431d65b9ac18ef
“dlgilligan” <danny.gilligan26@gmail>
2022-07-04 14:41:04 -0400

    7,8,9,10

--- lesson-7/boot-sect-main.asm
+++ lesson-7/boot-sect-main.asm
@@ -2,13 +2,13 @@
 ; cylinder: a cylindrical intersection through the stack of platters in a disk, centered around the disk's spindle
 ; head: a device on the arm of a hard drive used to read and write data on the disk platter
 ; sector: a subdivision of a track on a magnetic disk
-; carry bit: bit to registger when a carry or borrow has beenn generated
+; carry bit: an extra bit present on each register which stores when an operation has overflowed its current capacity:
 
 
 
 [org 0x7c00]
     mov bp, 0x8000 ; set the stack safely away from us
-    mov sp, bp
+    mov sp, bp ;stack is empty so top of stack points to bottom
 
     mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
     mov dh, 2 ; read 2 sectors
---
+++ lesson-8/32bit-print.asm
@@ -0,0 +1,32 @@
+; 32-bit protected mode: 32-bit mode allows us to use 32 bit registers and memory addressing, protected memory, 
+; virtual memory and other advantages, but we will lose BIOS interrupts
+;
+; VGA : an analog interface between computer and monitor
+; Video Memory : any type of random access memory (RAM) specifically used to store image data for a computer display
+
+[bits 32] ; using 32-bit protected mode
+
+; this is how constants are defined
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f ; the color byte for each character
+
+print_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY
+
+print_string_pm_loop:
+    mov al, [ebx] ; [ebx] is the address of our character
+    mov ah, WHITE_ON_BLACK
+
+    cmp al, 0 ; check if end of string
+    je print_string_pm_done
+
+    mov [edx], ax ; store character + attribute in video memory
+    add ebx, 1 ; next char
+    add edx, 2 ; next video memory position
+
+    jmp print_string_pm_loop
+
+print_string_pm_done:
+    popa
+    ret
